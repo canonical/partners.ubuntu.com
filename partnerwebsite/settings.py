96a81c595fa563ae64178804e388dbade67c5166
Tristram Oaten <tristram@oaten.name>
2014-05-21 17:48:50 +0100

    added u.qa to allowed hosts

--- partnerwebsite/settings.py
+++ partnerwebsite/settings.py
@@ -23,7 +23,9 @@ SECRET_KEY = '-q7g=b=d0($mr8vxb!_*-1aly29)v3@$ku(n5))z=orggymy9)'
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.ubuntu.qa'
+]
 
 
 # Application definition
