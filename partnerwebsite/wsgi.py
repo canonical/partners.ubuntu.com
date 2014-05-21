cb3cf6cfeb8fe660e8dba50c994033b5fb27b9d2
Tristram Oaten <tristram@oaten.name>
2014-05-21 16:17:05 +0100

    added path fix

--- partnerwebsite/wsgi.py
+++ partnerwebsite/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "partnerwebsite.settings")
 
 from django.core.wsgi import get_wsgi_application
