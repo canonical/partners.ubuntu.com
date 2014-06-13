883548357c61b7c9ab51d1ab43386c62e7350778
Tristram Oaten <tristram@oaten.name>
2014-06-13 16:45:15 +0100

    back to pg

--- partnerwebsite/settings.py
+++ partnerwebsite/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'partnerwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'partners',
         'USER': 'postgres',
         'PASSWORD': 'dev',
