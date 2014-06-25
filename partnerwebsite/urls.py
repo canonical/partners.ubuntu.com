96ce86fa2a9d006b317c81db303e27c5f577bd7f
Tristram Oaten <tristram@oaten.name>
2014-06-25 12:12:01 +0100

    added a slash

--- partnerwebsite/urls.py
+++ partnerwebsite/urls.py
@@ -61,7 +61,7 @@ def custom_500(request):
 urlpatterns = patterns(
     '',
     url(r'^admin', include(admin.site.urls)),
-    url(r'^partner-programmes$', PartnerView.as_view()),
+    url(r'^partner-programmes/?$', PartnerView.as_view()),
     url(r'^partner-programmes/(?P<name>[-\w]+)', partner_programmes),
     url(r'^$', PartnerView.as_view()),
     url(r'^contact-us$', PartnerView.as_view()),
