dcd112eb0bda950ad5bcfc0b3c032936c5f38506
Tristram Oaten <tristram@oaten.name>
2014-06-25 15:39:23 +0100

    fixed 500s

--- partnerwebsite/urls.py
+++ partnerwebsite/urls.py
@@ -49,13 +49,13 @@ class PartnerView(TemplateFinder):
 
 
 def custom_404(request):
-    t = loader.get_template('templates/404.html')
+    t = loader.get_template('404.html')
     context = RequestContext(request, {'request_path': request.path})
     return HttpResponseNotFound(t.render(context))
 
 
 def custom_500(request):
-    t = loader.get_template('templates/500.html')
+    t = loader.get_template('500.html')
     return HttpResponseServerError(t.render(Context({})))
 
 urlpatterns = patterns(
