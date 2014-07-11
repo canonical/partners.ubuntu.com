12fffb895b2ceb9979f043134686be54c9fe5e7c
Tristram Oaten <tristram@oaten.name>
2014-07-11 10:22:34 +0100

    text urls are no longer mandatory

---
+++ cms/migrations/0013_auto__chg_field_text_url.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Text.url'
+        db.alter_column(u'cms_text', 'url', self.gf('django.db.models.fields.URLField')(max_length=200, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Text.url'
+        db.alter_column(u'cms_text', 'url', self.gf('django.db.models.fields.URLField')(default='', max_length=200))
+
+    models = {
+        u'cms.industrysector': {
+            'Meta': {'object_name': 'IndustrySector'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'cms.insightstag': {
+            'Meta': {'object_name': 'InsightsTag'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'partner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Partner']"}),
+            'tag': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'cms.link': {
+            'Meta': {'object_name': 'Link'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'partner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Partner']"}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'cms.partner': {
+            'Meta': {'object_name': 'Partner'},
+            'created_by': ('django.db.models.fields.CharField', [], {'default': "'TEST_USER'", 'max_length': '200', 'blank': 'True'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'external_fallback': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'external_page': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'featured': ('django.db.models.fields.BooleanField', [], {}),
+            'generate_page': ('django.db.models.fields.BooleanField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'industry_sector': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'partners'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['cms.IndustrySector']"}),
+            'logo': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'long_description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'programme': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'partners'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['cms.Programme']"}),
+            'published': ('django.db.models.fields.BooleanField', [], {}),
+            'region': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'partners'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['cms.Region']"}),
+            'service_offered': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'partners'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['cms.ServiceOffered']"}),
+            'short_description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'technology': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'partners'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['cms.Technology']"}),
+            'updated_by': ('django.db.models.fields.CharField', [], {'default': "'TEST_USER'", 'max_length': '200', 'blank': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'cms.programme': {
+            'Meta': {'object_name': 'Programme'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'cms.quote': {
+            'Meta': {'object_name': 'Quote'},
+            'attribution': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'partner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Partner']"}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        u'cms.region': {
+            'Meta': {'object_name': 'Region'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'cms.serviceoffered': {
+            'Meta': {'object_name': 'ServiceOffered'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'cms.technology': {
+            'Meta': {'object_name': 'Technology'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'cms.text': {
+            'Meta': {'object_name': 'Text'},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'header': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'partner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Partner']"}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['cms']
