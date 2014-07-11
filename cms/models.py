f2d1e8e352f97eb268ceb0ace242352c93dc83c7
Tristram Oaten <tristram@oaten.name>
2014-07-11 16:44:45 +0100

    SSO user now added to audit partner

--- cms/models.py
+++ cms/models.py
@@ -11,7 +11,7 @@ class PartnerModel(models.Model):
     created_by = models.CharField(
         max_length=200,
         blank=True,
-        default="TEST_USER"
+        default=""
     )
     updated_on = models.DateTimeField(
         auto_now=True,
@@ -20,7 +20,7 @@ class PartnerModel(models.Model):
     updated_by = models.CharField(
         max_length=200,
         blank=True,
-        default="TEST_USER"
+        default=""
     )
     ordering = "name"
 
