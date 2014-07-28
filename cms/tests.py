54cc6c4154ce84a73a2fb0bedea8cde5c6ff313c
Tristram Oaten <tristram@oaten.name>
2014-07-28 11:49:20 +0100

    added simple api test

--- cms/tests.py
+++ cms/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.client import Client
+
+from cms.models import Partner
+
+c = Client()
+
+class APITestCase(TestCase):
+    def setUp(self):
+    	Partner.objects.create(
+    		name="Vendor", 
+    		slug="vendor",
+    		published=True,
+    		featured=True,
+    		generate_page=False
+    	)
+    	self.response = c.get('/partners.json', {'name': 'Vendor'})
+    	self.json_response = json.loads(self.response.content)
+
+    def test_(self):
+		self.assertEqual(self.json_response[0]['slug'], 'vendor')
