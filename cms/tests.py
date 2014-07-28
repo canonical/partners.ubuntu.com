f7c09b00234794373b44357f68e9546fa315e1c1
Tristram Oaten <tristram@oaten.name>
2014-07-28 18:06:55 +0100

    covered many more api tests, removed nose test runer from settings

--- cms/tests.py
+++ cms/tests.py
@@ -9,15 +9,33 @@ c = Client()
 
 class APITestCase(TestCase):
     def setUp(self):
-    	Partner.objects.create(
-    		name="Vendor", 
-    		slug="vendor",
-    		published=True,
-    		featured=True,
-    		generate_page=False
-    	)
-    	self.response = c.get('/partners.json', {'name': 'Vendor'})
-    	self.json_response = json.loads(self.response.content)
+        Partner.objects.create(
+            name="Vendor", 
+            slug="vendor",
+            published=True,
+            featured=True,
+            generate_page=False
+        )
 
     def test_get_slug(self):
-		self.assertEqual(self.json_response[0]['slug'], 'vendor')
+        response = c.get('/partners.json', {'name': 'Vendor'})
+        json_response = json.loads(response.content)
+        self.assertEqual(json_response[0]['slug'], 'vendor')
+
+    def test_get_all(self):
+        response = c.get('/partners.json')
+        json_response = json.loads(response.content)
+        self.assertEqual(json_response[0]['slug'], 'vendor')
+
+    def test_get_nothing(self):
+        response = c.get('/partners.json', {'name': 'nothing-here'})
+        json_response = json.loads(response.content)
+        self.assertEqual(len(json_response), 0)
+
+    def test_bad_querystring(self):
+        response = c.get('/partners.json', {'nothing': 'nothing-here'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_bad_type(self):
+        response = c.get('/partners.json', {'programme': 'nothing-here'})
+        self.assertEqual(response.status_code, 400)
