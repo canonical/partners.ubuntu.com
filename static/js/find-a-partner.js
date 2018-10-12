e22746128280ec0701034b30f0b78dd3b4f67f74
Anthony Dillon <me@anthonydillon.com>
2018-10-12 23:07:45 +0100

    Add search functionality to the find a partner page

--- static/js/find-a-partner.js
+++ static/js/find-a-partner.js
@@ -1,127 +1,81 @@
 (function() {
+
   function init() {
+    revealSearch();
+
     var inputNode = document.querySelector(".p-find-a-partner__search-input");
-    var minQueryLength = 0;
-    var queryDelay = 0;
+    inputNode.addEventListener("keyup", searchHandler);
 
-    var results = [];
-
-    inputNode.addEventListener("keydown", search);
-
-    var partners = document.querySelectorAll(".p-find-a-partner__partner");
-    partners.forEach(function(node) {
-      results.push({
-        node: node,
-        searchText: node.getAttribute("data-searchText")
-      });
+    var checkboxes = document.querySelectorAll(".p-find-a-partner__filter");
+    checkboxes.forEach(function(checkbox) {
+      checkbox.addEventListener("change", filter);
     });
 
-    var resultTextLocator = "searchText";
-
-    // filter on part-words
-    var resultFilters = "subWordMatch";
+    populateCheckboxes();
+    populateTextbox();
   }
 
-  // Returns true if there are any matched results
   function matchesExist() {
     var numberOfPartners = document.querySelectorAll(
       ".p-find-a-partner__partner"
     ).length;
-    var numberOfSearchMisses = document.querySelectorAll(".not-search-match")
-      .length;
-    var numberOfFilterMisses = document.querySelectorAll(".not-filter-match")
-      .length;
-    if (
-      numberOfSearchMisses == numberOfPartners ||
-      numberOfFilterMisses == numberOfPartners
-    ) {
+    var numberOfHidden = document.querySelectorAll(
+      ".p-find-a-partner__partner.u-hide"
+    ).length;
+    if (numberOfHidden === numberOfPartners) {
       return false;
     } else {
       return true;
     }
   }
 
-  function updateNoResultsMessage(matchesExist) {
-    if (matchesExist) {
-      var partnerGood = document.querySelector(".p-find-a-partner__no-results");
-      partnerGood.classList.add("u-hide");
+  function updateNoResultsMessage() {
+    var noResults = document.querySelector(".p-find-a-partner__no-results");
+    if (matchesExist()) {
+      noResults.classList.add("u-hide");
     } else {
-      var partnerBad = document.querySelector(".p-find-a-partner__no-results");
-      partnerBad.classList.remove("u-hide");
+      noResults.classList.remove("u-hide");
     }
   }
 
-  function cloneItem(node, exact) {
-    var container = document.querySelector(
-      ".p-find-a-partner__prioritised-results"
-    );
-    clone = node.cloneNode(true);
-    node.classList.add("match-hide");
-    if (exact) {
-      container.prepend(clone);
-    } else {
-      container.append(clone);
-    }
+  function searchHandler(e) {
+    var query = e.target.value;
+    search(query);
   }
 
-  function prioritiseTitleMatches(search) {
-    //clone any exact title matches and hide the original
-    var partners = document.querySelectorAll(".p-find-a-partner__partner");
-
-    partners.forEach(function(partner) {
-      partnerTitle = partner.getAttribute("ID");
-      if (partnerTitle == search) {
-        cloneItem(partner, true);
-      } else {
-        if (partnerTitle.indexOf(search) != -1) {
-          cloneItem(partner, false);
-        }
-      }
-    });
-  }
-
-  // Subscribe to the "results" event
-  function search(e) {
-    console.log("start", e.target.value);
-    var partnerList = document.querySelector(".p-find-a-partner__partner-list");
-    partnerList.classList.add("u-hide");
+  function search(query) {
     var partners = document.querySelectorAll(".p-find-a-partner__partner");
     partners.forEach(function(partner) {
       partner.classList.add("u-hide");
     });
-    var priorityResults = document.querySelector(
-      ".p-find-a-partner__prioritised-results"
-    );
-    var results = document.querySelectorAll(".p-find-a-partner__partner");
-    partners.forEach(function(partner) {
-      partner.classList.remove("u-hide");
+
+    var partners = document.querySelectorAll(".p-find-a-partner__partner");
+
+    partners.forEach(function(node) {
+      var searchText = node.getAttribute("data-searchText").toLowerCase();
+      if (searchText.includes(query)) {
+        node.classList.remove("u-hide");
+      }
     });
-    prioritiseTitleMatches(e.query);
-    updateNoResultsMessage(matchesExist());
+    updateNoResultsMessage();
   }
 
-  //Adds a listener to checkboxes to filter results
-  var checkboxes = document.querySelectorAll(".p-find-a-partner__filter");
-  checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener("change", function(e) {
-      console.log("change");
-      var checkbox = e.target;
-      var checked = checkbox.checked;
-      var attributeName = checkbox.id;
-      updateFilter(attributeName, checked);
-      updateNoResultsMessage(matchesExist());
-    });
-  });
+  function filter(e) {
+    var checkbox = e.target;
+    var checked = checkbox.checked;
+    var attributeName = checkbox.id;
+    updateFilter(attributeName, checked);
+    updateNoResultsMessage();
+  }
 
-  var filters = [];
   //Adds the provided filter and filters the results accordingly
   function updateFilter(name, add) {
+    var filters = [];
     filters[name] = add;
     partners = document.querySelectorAll(".p-find-a-partner__partner");
     partners.forEach(function(node) {
       node.classList.add("u-hide");
-    });
-    partners.forEach(function(node) {
+
       dataFilter = node.getAttribute("data-filter");
       for (var name in filters) {
         if (filters[name] == true && dataFilter.indexOf(name) != -1) {
@@ -197,24 +151,23 @@
     }
   }
 
-  //auto-fill text search
   function populateTextbox() {
     var searchbox = document.querySelector(".p-find-a-partner__search-input");
     var searchText = getParameterByName("search");
-    if (searchbox != null && searchText != null) {
+    if (searchbox && searchText) {
       searchbox.focus();
       searchbox.value = searchText;
+      search(searchText);
     }
   }
 
-  populateCheckboxes();
-  populateTextbox();
+  function revealSearch() {
+    var removeSearch = document.querySelector(
+      ".p-find-a-partner.u-hide"
+    );
+    removeSearch.classList.remove("u-hide");
+  }
+
   init();
 
-  var removeSearch = document.querySelectorAll(
-    ".p-find-a-partner js-search-not-run"
-  );
-  removeSearch.forEach(function(removed) {
-    removed.classList.remove("js-search-not-run");
-  });
 })();
