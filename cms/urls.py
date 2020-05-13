b10c7b968667489790d53becda003a95d4aeb78c
Thomas Bille <toto@canonical.com>
2020-05-13 15:26:27 +0100

    Remove all public pages

--- cms/urls.py
+++ cms/urls.py
@@ -8,10 +8,6 @@ from django.views.generic.base import RedirectView
 from django_yaml_redirects import load_redirects
 
 from cms.views import (
-    partner_programmes,
-    partner_view,
-    PartnerView,
-    find_a_partner,
     partners_json_view,
     customers_json_view,
 )
@@ -45,23 +41,4 @@ urlpatterns += [
     url(r"^admin/?", include(admin.site.urls)),
     url(r"^partners.json$", partners_json_view),
     url(r"^customers.json$", customers_json_view),
-    url(r"^programmes/?$", PartnerView.as_view()),
-    url(
-        r"^partner-programmes/?$",
-        RedirectView.as_view(url="/programmes/", permanent=True),
-    ),
-    url(
-        r"^partner-programmes/(?P<name>[-\w]+)",
-        lambda request, name: RedirectView.as_view(
-            url="/programmes/" + name, permanent=True
-        )(request),
-    ),
-    url(r"^programmes/(?P<name>[-\w]+)", partner_programmes),
-    url(r"^$", PartnerView.as_view()),
-    url(r"^contact-us$", PartnerView.as_view()),
-    url(r"^thank-you$", PartnerView.as_view()),
-    url(r"^request-login$", PartnerView.as_view()),
-    url(r"^find-a-partner$", find_a_partner),
-    url(r"^partnering-with-us$", PartnerView.as_view()),
-    url(r"^(?P<slug>[-\w]+)/?$", partner_view),
 ]
