5c10f8710cced58ba8ad41038928ee018702333f
Samuel Olwe <samuel.olwe@canonical.com>
2023-05-23 20:29:58 +0300

    Removed unused security middleware

--- cms/settings.py
+++ cms/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = (
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
